17dcafc52bc37b6426ca8b8e86d7abf8a0b60ef3
Bern <bernnystrom@gmail.com>
2022-08-23 12:38:48 +0800

    🏗️ (new): Create & define app model structure with sample data
    Created the app model based on the JSON data structure the app will be decoding its data from and added a temporary mock data object for testing purposes.

---
+++ Treats/Treat.swift
@@ -0,0 +1,38 @@
+//
+//  Treat.swift
+//  Treats
+//
+//  Created by Bern N on 8/23/22.
+//
+
+import Foundation
+
+struct Treat: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct TreatResponse {
+    let request: [Treat]
+}
+
+struct MockData {
+    static let sampleTreat = Treat(
+        id: 0001,
+        name: "Sample Treat",
+        description: "Description for the yummy sample treat.",
+        price: 9.99,
+        imageURL: "",
+        calories: 175,
+        protein: 50,
+        carbs: 15
+    )
+    
+    static let treats = [sampleTreat, sampleTreat, sampleTreat, sampleTreat]
+}
