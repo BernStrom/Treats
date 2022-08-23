cc8a6f8e0e3f267246f71e54b531066c036f9426
Bern <bernnystrom@gmail.com>
2022-08-23 13:32:13 +0800

    🏗️ (chore): Add Identifiable conformance to app model
    Modify the model object by adding Identifiable to its conformance protocol.

--- Treats/Treat.swift
+++ Treats/Treat.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Treat: Decodable {
+struct Treat: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
---
+++ Treats/TreatListCell.swift
@@ -0,0 +1,20 @@
+//
+//  TreatListCell.swift
+//  Treats
+//
+//  Created by Bern N on 8/23/22.
+//
+
+import SwiftUI
+
+struct TreatListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TreatListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TreatListCell()
+    }
+}
