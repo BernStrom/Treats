617ff59ebc268de00885594039bf0c7a806953b7
Bern <bernnystrom@gmail.com>
2022-08-26 15:34:42 +0800

    ✨ (new): Set food detail view to display on user tap
    Added the functionality where users tapped on any of the food in the list, a detail view of that selected food will display on screen like a popup view, with a blur effect set on the background. For UX optimization, interaction on the background is disabled until the display of the food detail view is dismissed by the user.

--- Treats/Screens/TreatListView/TreatsListView.swift
+++ Treats/Screens/TreatListView/TreatsListView.swift
@@ -9,17 +9,29 @@ import SwiftUI
 
 struct TreatsListView: View {
     @StateObject var viewModel = TreatsListViewModel()
+    @State private var selectedTreat: Treat?
+    @State private var isShowingDetailView = false
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.treats) { treat in
                     TreatListCell(treat: treat)
+                        .onTapGesture {
+                            selectedTreat = treat
+                            isShowingDetailView = true
+                        }
                 }
                 .navigationTitle("🥞 treats.")
+                .disabled(isShowingDetailView)
             }
             .onAppear {
                 viewModel.getTreats()
+            }
+            .blur(radius: isShowingDetailView ? 20 : 0)
+            
+            if isShowingDetailView {
+                TreatDetailView(treat: selectedTreat ?? MockData.sampleTreat, isShowingDetailView: $isShowingDetailView)
             }
             
             if viewModel.isLoading {
