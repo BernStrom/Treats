c7ec07e3463dc75152893ded43e8651473966330
Bern <bernnystrom@gmail.com>
2022-08-28 15:32:18 +0800

    💄 (new): Set up UI layout for account's screen
    Finished setting up the account view UI layout with the required @State properties for the form components; TextField, DatePicker, etc.

--- Treats/Screens/AccountView/AccountView.swift
+++ Treats/Screens/AccountView/AccountView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("🎅 Account")
+            Form {
+                Section("Personal Info") {
+                    TextField("First Name", text: $firstName)
+                        .textContentType(.givenName)
+                    
+                    TextField("Last Name", text: $lastName)
+                        .textContentType(.familyName)
+                    
+                    TextField("Email", text: $email)
+                        .textContentType(.emailAddress)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .disableAutocorrection(true)
+                    
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Changes saved")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section("Requests") {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: Color("AccentColor")))
+            }
+            .navigationTitle("🎅 Account")
         }
     }
 }
