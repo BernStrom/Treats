176cfd3389c3a428e85d343d5778c8aeec9d4433
Bern <bernnystrom@gmail.com>
2022-08-29 14:39:58 +0800

    🥅 (new): Add alert contexts for user account
    Added two additional alert contexts for user account related cases; userSaveSuccess - when user profile was successfully saved & invalidUserData - when there was a problem saving or retrieving user profile.

--- Treats/Utilities/Alerts/Alert.swift
+++ Treats/Utilities/Alerts/Alert.swift
@@ -55,4 +55,16 @@ struct AlertContext {
         message: Text("Please ensure your email is in a legible format."),
         dismissButton: .default(Text("OK"))
     )
+    
+    static let userSaveSuccess = AlertItem(
+        title: Text("Profile Saved"),
+        message: Text("Your profile information was successfully saved."),
+        dismissButton: .default(Text("OK"))
+    )
+    
+    static let invalidUserData = AlertItem(
+        title: Text("Profile Error"),
+        message: Text("There was a problem while saving or retrieving your profile."),
+        dismissButton: .default(Text("OK"))
+    )
 }
