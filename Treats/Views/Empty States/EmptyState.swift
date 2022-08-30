62880b67ab20edb33f3fa95870596f6207be93e6
Bern <bernnystrom@gmail.com>
2022-08-30 15:00:23 +0800

    💄 (new): Add an empty view for displaying an empty state UI to users
    Created a new view - EmptyState that will be used to display the UI of the app's empty state to the user. This is created to be used for the order view's state, but its a view that can be reused for displaying different types empty states UI on screen.

---
+++ Treats/Views/Empty States/EmptyState.swift
@@ -0,0 +1,40 @@
+//
+//  EmptyState.swift
+//  Treats
+//
+//  Created by Bern N on 8/30/22.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3.weight(.semibold))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+            .offset(y: -50)
+        }
+    }
+}
+
+struct EmptyState_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyState(imageName: "empty-order", message: "This is a preview test message.\nFor development purposes only.")
+    }
+}
