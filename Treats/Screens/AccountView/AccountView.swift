89a02138b0848f3a38ac0bb62bef2cf3ba232f81
Bern <bernnystrom@gmail.com>
2022-09-01 23:28:35 +0800

    ♻️ (refactor): Change code formatting of .onAppear() action
    Refactored the code format of the .onAppear action to a single line with its (perform:) keyword.

--- Treats/Screens/AccountView/AccountView.swift
+++ Treats/Screens/AccountView/AccountView.swift
@@ -61,9 +61,7 @@ struct AccountView: View {
             }
             .navigationTitle("🎅 Account")
         }
-        .onAppear {
-            viewModel.retrieveUser()
-        }
+        .onAppear(perform: viewModel.retrieveUser)
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
